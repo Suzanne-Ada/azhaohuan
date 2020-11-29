d1e25ec6f852a94a0c20b5a7207161be3ff144c5
summer <1147571275@qq.com>
2020-11-29 14:41:52 +0800

    second assessment

---
+++ 1909020014赵焕2/VARMILO.js
@@ -0,0 +1,10 @@
+var nava = document.querySelector('.nava')
+var lis = nava.childern;
+for (var i = 0; i < lis.length; i++) {
+    lis[i].onmouseover = function() {
+        this.children[1].style.display = 'block';
+    }
+    lis[i].onmouseout = function() {
+        this.children[1].style.display = 'none';
+    }
+}
